3a114262edf6cf5ff3217ba90fb792459d75273f
sanmathisrinath <sanmathi28@gmail.com>
2020-02-06 13:02:38 +0530

    Commented pandas code, imported os module

--- pandas_trial.py
+++ pandas_trial.py
@@ -1,9 +1,11 @@
-import pandas as pd
-
-cars = {'Brand': ['Honda Civic','Toyota Corolla','Ford Focus','Audi A4'],
-        'Price': [22000,25000,27000,35000]
-        }
-
-df = pd.DataFrame(cars, columns = ['Brand', 'Price'])
-
-print(df)
+# import pandas as pd
+#
+# cars = {'Brand': ['Honda Civic','Toyota Corolla','Ford Focus','Audi A4'],
+#         'Price': [22000,25000,27000,35000]
+#         }
+#
+# df = pd.DataFrame(cars, columns = ['Brand', 'Price'])
+#
+# print(df)
+import os
+print(os.curdir)
